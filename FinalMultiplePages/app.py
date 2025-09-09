6257a211edb479cb7800e6810ace4245981e1b56
dcrobbins1 <dcrobbins@wm.edu>
2025-09-09 10:32:14 -0400

    Test

---
+++ FinalMultiplePages/app.py
@@ -0,0 +1,26 @@
+import dash
+import dash_bootstrap_components as dbc
+from dash import html, dcc
+
+# Initialize app with pages support
+app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.FLATLY])
+server = app.server
+
+app.layout = dbc.Container([
+    html.H1("Food & Drinks Explorer", className="text-center my-3"),
+    dbc.Nav(
+        [
+            dbc.NavLink("Home", href="/", active="exact"),
+            dbc.NavLink("About", href="/about",active="exact"),
+        ],
+        pills=True,
+        justified=True,
+        className="mb-4"
+    ),
+    dash.page_container
+], fluid=True)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
